6f3cec18b9737ef43872c9e3034fae8eed0179dc
516lkh <516rudgus@gmail.com>
2023-09-11 14:20:12 +0900

    AutoMover 수정 (등속도운동 / 반사각보정)

--- Assets/Scripts/Components/AutoMover.cs
+++ Assets/Scripts/Components/AutoMover.cs
@@ -8,20 +8,46 @@ using UnityEngine;
 public class AutoMover : PooledObject
 {
     public Vector2 accelate;
+    public int speed;
     private Rigidbody2D rigid;
 
     private void Awake()
     {
+        speed = 4;
         rigid = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
-        rigid.velocity = accelate;
+        rigid.velocity = accelate.normalized * speed;
     }
 
     private void OnEnable()
     {
-        rigid.velocity = accelate;
+        rigid.velocity = accelate.normalized * speed;
+    }
+
+
+    void Update()
+    {
+        if (rigid.velocity != Vector2.zero)
+        {
+            rigid.velocity = rigid.velocity.normalized * speed;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (rigid.velocity.y < speed * 0.1 && rigid.velocity.y > speed * (-0.1))
+        {
+            if (rigid.velocity.y <= 0)
+            {
+                rigid.velocity += new Vector2(0, -0.3f) * speed;
+            }
+            if (rigid.velocity.y > 0)
+            {
+                rigid.velocity += new Vector2(0, 0.3f) * speed;
+            }
+        }
     }
 }
