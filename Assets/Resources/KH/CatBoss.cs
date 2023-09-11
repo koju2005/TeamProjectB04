74d3648ba44bc096a33f3247a917faeb0e6118e3
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-09-11 18:43:02 +0900

    [ADD]KHScene추가

---
+++ Assets/Resources/KH/CatBoss.cs
@@ -0,0 +1,35 @@
+using DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class CatBoss : Shooter
+{
+    public float MoveSpeed;
+    private Health PatternHealth;
+    private void Start()
+    {
+        PatternHealth = GetComponent<Health>();
+        PatternHealth.OnHealthChanged += BossPattern;
+        StartRoutine();
+    }
+    private void Update()
+    {
+        BossPattern();
+    }
+
+    private void BossPattern()
+    {
+        
+        if (PatternHealth.GetHealth() < 5)
+        {
+            if (transform.position.x > 2.5 || transform.position.x < -2.5)
+            {
+                MoveSpeed = -MoveSpeed;
+            }
+            transform.position += new Vector3(MoveSpeed, 0, 0);
+        }
+        
+    }
+}
