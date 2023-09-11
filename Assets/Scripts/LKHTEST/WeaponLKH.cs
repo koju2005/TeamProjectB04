aca63baa63d6b7714826500c16d715f279203ff0
516lkh <516rudgus@gmail.com>
2023-09-11 13:57:03 +0900

    테스트 환경

---
+++ Assets/Scripts/LKHTEST/WeaponLKH.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+namespace DefaultNamespace.Data
+{
+    [RequireComponent(typeof(Collisionable),typeof(AutoMover),typeof(PooledObject))]
+    public class WeaponLKH: MonoBehaviour
+    {
+        [SerializeField] private int _damage;
+        public int Damage
+        {
+            get { return _damage;}
+            private set { _damage = value; }
+        }
+
+        public void OnEnable()
+        {
+            if (gameObject.layer == LayerMask.NameToLayer("Projectile"))
+            {
+                GameManager.Instance.AddWeapon(gameObject);                
+            }
+            
+        }
+
+        public void OnDisable()
+        {
+            try
+            {
+                if (!GameManager.isApplicationExit || gameObject.layer == LayerMask.NameToLayer("Projectile"))
+                {
+                    if (GameManager.Instance != null)
+                        GameManager.Instance.RemoveWeapon(gameObject);                
+                    else
+                        Destroy(gameObject);                
+                }
+            }
+            catch(Exception e)
+            {
+                
+            }
+           
+        }
+    }
+}
