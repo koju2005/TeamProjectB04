4c201eeabadd02e54c41f2ebafcae41d50419ef5
516lkh <516rudgus@gmail.com>
2023-09-12 22:18:43 +0900

    기본 에셋 설정

---
+++ Assets/Scripts/AIScript/LKHAI.cs
@@ -0,0 +1,14 @@
+using DefaultNamespace;
+using DefaultNamespace.Common;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LKHAI : MonoBehaviour
+{
+
+
+
+    
+}
