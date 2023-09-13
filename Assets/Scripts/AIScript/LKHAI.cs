5d7333cd9f0cab93fc399e8d86954f98a8f8dc50
516lkh <516rudgus@gmail.com>
2023-09-13 16:32:31 +0900

    보스 애니메이션 완료

--- Assets/Scripts/AIScript/LKHAI.cs
+++ Assets/Scripts/AIScript/LKHAI.cs
@@ -25,13 +25,16 @@ public class LKHAI : MonoBehaviour
     private Vector3[] spawnPos = new Vector3[8];
     private GameObject[] isMonsterDie = new GameObject[8];
 
+
     private void Awake()
     {
         _health = GetComponent<Health>();
         _health.OnHealthChanged += Interrupt;
+
         _health.OnHealthChanged += Hit_anim;
         _SpriteRenderer = GetComponentInChildren<SpriteRenderer>();
         _anim = _SpriteRenderer.GetComponent<Animator>();
+
     }
 
     private void Start()
@@ -97,7 +100,7 @@ public class LKHAI : MonoBehaviour
         else if (_health.GetHealthRate() <= 0.5f && phaseLevel <= 2)
         {
             StopCoroutine(currentPhase);
-            currentPhase = StartCoroutine(phase5());
+            currentPhase = StartCoroutine(phase3());
         }
         else if (_health.GetHealthRate() <= 0.8f && phaseLevel <= 1)
         {
@@ -111,11 +114,8 @@ public class LKHAI : MonoBehaviour
     {
         phaseLevel = 6;
         Time.timeScale = 0;
-        _dialogTyper.Enqueue("아..아...");
-        _dialogTyper.Enqueue("아..아...");
-        _dialogTyper.Enqueue(".. 나의 .. 꿈은...");
-        _dialogTyper.Enqueue("나의 휴가는.....");
-        _dialogTyper.Enqueue("나의 휴일은.....");
+        _dialogTyper.Enqueue("거의 다 도망쳤는데...");
+        _dialogTyper.Enqueue("이 망할자식..!!");
         yield return new WaitUntil(() => !_dialogTyper.isSbWrite);
 
         for (int i = 0; i < spawnPos.Length; ++i)
@@ -128,13 +128,9 @@ public class LKHAI : MonoBehaviour
 
         }
 
-        int alpha = 255;
-        while (alpha > 0)
-        {
-            alpha -= (int)(255 * Time.unscaledDeltaTime);
-            _SpriteRenderer.color = new Color(0, 0, 0, alpha);
-            yield return null;
-        }
+        _anim.SetBool("isDead", true);
+        _dialogTyper.Enqueue("끄아아아아아아악!!!!");
+
         yield return CoroutineTime.GetWaitForSecondsRealtime(7f);
         Time.timeScale = 1;
         _health.AddHealth(-100);
@@ -145,7 +141,8 @@ public class LKHAI : MonoBehaviour
         phaseLevel = 1;
         Time.timeScale = 0;
         yield return CoroutineTime.GetWaitForSecondsRealtime(1);
-        _dialogTyper.Enqueue("여기까지 \n잘 오셨습니다.");
+        _dialogTyper.Enqueue("벌써 여기까지 쫒아왔나..");
+        _dialogTyper.Enqueue("네놈을 박살내주마!");
         SpawnMonster("Bat", MonsterSpawnPos.B1);
         SpawnMonster("Bat", MonsterSpawnPos.B2);
         SpawnMonster("Bat", MonsterSpawnPos.B3);
@@ -160,14 +157,11 @@ public class LKHAI : MonoBehaviour
     {
         phaseLevel = 2;
 
-        _dialogTyper.WriteNow("으윽!");
+        _dialogTyper.WriteNow("으악!");
         yield return CoroutineTime.GetWaitForSecondsRealtime(1.5f);
 
         Time.timeScale = 0;
-        _dialogTyper.Enqueue("약하시진 않군요");
-        _dialogTyper.Enqueue("역시 사신님");
-        _dialogTyper.Enqueue("이제 제대로\n가겠습니다");
-        _dialogTyper.Enqueue("자 가자 얘들아!!");
+        _dialogTyper.Enqueue("곱게 잡히진 않겠다!");
         yield return new WaitUntil(() => !_dialogTyper.isSbWrite);
         Time.timeScale = 1;
         SpawnMonster("FireGuy", MonsterSpawnPos.RIGHT);
@@ -183,17 +177,14 @@ public class LKHAI : MonoBehaviour
     }
 
 
-    private IEnumerator phase5()
+    private IEnumerator phase3()
     {
-        phaseLevel = 5;
+        phaseLevel = 3;
 
-        _dialogTyper.WriteNow("아아아아앜!");
-        yield return CoroutineTime.GetWaitForSecondsRealtime(1.5f);
-        _dialogTyper.WriteNow("아아아아아앜!!");
-        yield return CoroutineTime.GetWaitForSecondsRealtime(1.5f);
-        _dialogTyper.WriteNow("아아아아아아앜!!");
+        _dialogTyper.WriteNow("죽어라!!");
         yield return CoroutineTime.GetWaitForSecondsRealtime(1.5f);
 
+        _anim.SetTrigger("isAttack");
         NShooter shooter = transform.AddComponent<NShooter>();
         shooter.AttackPrefabsName = new[] { "Dongs1Weapon", "Dongs2Weapon" };
         shooter.AttackDelay = new[] { 5.0f };
@@ -242,8 +233,6 @@ public class LKHAI : MonoBehaviour
             yield return CoroutineTime.GetWaitForSeconds(phase4RespawnTime);
         }
     }
-
-
 
 
 }
